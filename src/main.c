a713a38a1b67f9d23f91dbc15213da3be10884f1
oasdflkjo <petri.pihla@gmail.com>
2024-11-03 13:06:23 +0200

    refactor

--- src/main.c
+++ src/main.c
@@ -80,8 +80,19 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_H && action == GLFW_PRESS) {
-        ui_toggle(&world.ui);
+    if (action != GLFW_PRESS)
+        return;
+
+    switch (key) {
+        case GLFW_KEY_H:
+            ui_toggle(&world.ui);
+            break;
+        case GLFW_KEY_ESCAPE:
+            glfwSetWindowShouldClose(window, true);
+            break;
+        case GLFW_KEY_SPACE:
+            camera_reset(&camera);
+            break;
     }
 }
 
@@ -111,11 +122,6 @@ int main() {
         return -1;
     }
 
-    // thanks for subscribing <3
-    // it means a lot to me
-
-    
-
     // Setup window
     glfwSetWindowPos(window, 0, 0);
     glfwMakeContextCurrent(window);
@@ -136,11 +142,6 @@ int main() {
     glGetIntegerv(GL_MINOR_VERSION, &minorVersion);
     printf("OpenGL Version: %d.%d\n", majorVersion, minorVersion);
 
-    if (majorVersion < 4 || (majorVersion == 4 && minorVersion < 3)) {
-        fprintf(stderr, "OpenGL 4.3 or higher is required for compute shaders\n");
-        return -1;
-    }
-
     // Initialize camera and world
     camera_init(&camera, windowWidth, windowHeight);
     world_init(&world, window);
@@ -149,23 +150,14 @@ int main() {
     glfwSetKeyCallback(window, key_callback);
 
     // Main loop
-    const double targetFrameTime = 1.0 / 60.0;  // For 160 FPS
+    const double targetFrameTime = 1.0 / 60.0;  // For 60 FPS
     
     while (!glfwWindowShouldClose(window)) {
-        double frameStart = glfwGetTime();
-        
-        // Get delta time
+
         static float lastFrame = 0.0f;
-        float currentFrame = glfwGetTime();
+        double currentFrame = glfwGetTime();
         float deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
-
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(window, true);
-
-        if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-            camera_reset(&camera);  // This will now trigger smooth transition
-        }
 
         // Update camera zoom and position
         camera_update(&camera, deltaTime);
@@ -173,17 +165,11 @@ int main() {
         world_render(&world, &camera);
 
         glfwSwapBuffers(window);
-
-        // Add frame time measurement here
-        static double lastRenderTime = 0.0;
-        double currentTime = glfwGetTime();
-        //printf("Frame time: %.2f ms\n", (currentTime - lastRenderTime) * 1000.0);
-        lastRenderTime = currentTime;
         
         glfwPollEvents();
         
         // Frame limiting
-        while (glfwGetTime() - frameStart < targetFrameTime) {
+        while (glfwGetTime() - currentFrame < targetFrameTime) {
             // Busy-wait or could use a sleep for better CPU usage
         }
     }
